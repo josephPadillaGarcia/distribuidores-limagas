fdfa51446f2748716e346c480380b015bfc3b717
Anthony T <anthony527@gmail.com>
2021-12-25 17:13:07 -0500

    Actualizado Pagina GeneralInformation

--- resources/views/admin/pages/content/general-information.blade.php
+++ resources/views/admin/pages/content/general-information.blade.php
@@ -3,7 +3,6 @@
 
     <content-general-information
     route-update="{{ route('cms.content.general-information.store') }}" 
-    :departments="{{ json_encode($departments) }}"
     route-get="{{ route('cms.content.general-information.get') }}"></content-general-information>
     
 @endsection
