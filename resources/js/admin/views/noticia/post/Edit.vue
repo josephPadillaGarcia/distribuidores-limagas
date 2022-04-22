5df609122e878481c1190456bfbd68bc3458359d
Anthony <anthto527@gmail.com>
2022-04-22 16:14:01 -0500

    Actualizado Editar Post Insertar Imagen

--- resources/js/admin/views/noticia/post/Edit.vue
+++ resources/js/admin/views/noticia/post/Edit.vue
@@ -139,7 +139,7 @@
                       :valueEs.sync="element.content_es"
                       :valueEnParent="element.content_en"
                       :valueEsParent="element.content_es"
-                      :url="'posts'"
+                      :url="'/admin/blog/posts/posts'"
                       :text-image="'image'"
                     />
                   </div>
